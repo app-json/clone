156e62fb89cab02d6c2c1743b6141795dba1c75e
zeke <zeke@sikelianos.com>
2014-05-15 10:05:28 -0700

    get apps from the wiki registry

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,49 +1,27 @@
-var apps = []
-var repos = []
-var repo = location.pathname.match(/^\/apps\/(.*)\/(.*)/)
-
-// If url is in the format /apps/user/repo, then use that repo
-// Otherwise, use a canned list of repos
-if (repo) {
-  repos.push([repo[1], repo[2]].join("/"))
-} else {
-  repos.push(
-    'heroku/node-js-sample',
-    'heroku/ruby-rails-sample',
-    'kennethreitz/httpbin',
-    'ryanbrainard/forceworkbench',
-    'zeke/harp-slideshow-template',
-    'zeke/hound',
-    'zeke/chaos-monkey-app-manifest',
-    'zeke/roots-buildpack-sample',
-    'zeke/moby',
-    'zeke/goinstant-buildpack-example'
-  )
-}
+window.apps = []
 
 $(function() {
-  var repo, i
 
-  // Iterate over shorthand repo names, fetching app.json content for each
-  for (i = 0; i < repos.length; i++) {
-    repo = repos[i]
-    App.fetch(repo, function(err, app) {
-      if (err) return console.error(app, err)
-      if (!app.valid) return console.error(app, app.errors)
-      apps.push(app)
+  $.getJSON("http://app-registry.herokuapp.com/apps", function(urls) {
 
-      app.getAddonPrices(function(err, prices) {
-        if (err) return console.error(err)
-        app.prices = prices
-        $('#apps').append(App.templates.app.render(app))
-
-        if (repos.length === 1) {
-          $('#apps .app').addClass('active')
-        }
+    console.log(urls)
+    
+    urls.forEach(function(url){
+      App.fetch(url, function(err, app) {
+        if (err) return console.error(app, err)
+        if (!app.valid) return console.error("Invalid app", app, app.errors)
+        apps.push(app)
+        app.getAddonPrices(function(err, prices) {
+          if (err) return console.error(app, err)
+          app.prices = prices
+          $('#apps').append(App.templates.app.render(app))
+          // if (urls.length === 1) $('#apps .app').addClass('active')
+        })
       })
-
     })
-  }
+
+  })
+
 
   // When an app is clicked, display a "deploy" button
   $(".app a.activator").live("click", function(event) {
